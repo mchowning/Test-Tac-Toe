4caf6b05db0cae5260db6c75c51b9fb5511da51e
Matt Chowning <matt.chowning@detroitlabs.com>
2014-03-31 12:41:03 -0400

    Initial commit with only empty project and Kiwi pod installation.

---
+++ TestTacToe/TTTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTTAppDelegate.h
+//  TestTacToe
+//
+//  Created by Matt Chowning on 3/31/14.
+//  Copyright (c) 2014 Matt Chowning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestTacToe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestTacToe
+//
+//  Created by Matt Chowning on 3/31/14.
+//  Copyright (c) 2014 Matt Chowning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TTTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TTTAppDelegate class]));
+    }
+}
